99d5157c43cb2cbd1df007e09937e84022f61556
feather <Brian608@163.com>
2021-12-26 20:48:58 +0800

    递归

---
+++ src/org/feather/algorithm/recursion.java
@@ -0,0 +1,22 @@
+package org.feather.algorithm;
+
+/**
+ * @program: algorithm
+ * @description:递归
+ * @author: 杜雪松(feather)
+ * @since: 2021-12-26 20:23
+ **/
+public class recursion {
+    public static void main(String[] args) {
+        System.out.println(f(10));
+    }
+    public  static  long f(int n){
+        if (n<1){
+            return  -1;
+        }
+        if (n==1){
+            return  1;
+        }
+        return  f(n-1)+n;
+    }
+}
